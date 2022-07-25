e0949d3720e8872d5753182d12dce6d145d86caf
jwelbeck <jwelbeck38@gmail.com>
2022-07-25 16:20:21 +0000

    update shell scripts

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,20 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="jwelbeck/mlapp"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl create deploy mlapp --image=$dockerpath
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+podname=$(kubectl get pods --no-headers -o custom-columns=":metadata.name")
+kubectl port-forward $podname 8000:80
+kubectl logs $podname
 
